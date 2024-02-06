47baa1b404c476c7e3fdf19fcb6e52bdc450ec8e
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-02-06 18:58:39 +0700

    Membuat component produk component unutk menampilkan prdoduk yang tersedia dalam tabel

---
+++ resources/js/components/ProductComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Stock</th>
+            <th>Price</th>
+        </tr>
+        <tr v-for="(prod, index) in list" :key="index">
+            <td>{{ prod.name }}</td>
+            <td>{{ prod.description }}</td>
+            <td>{{ prod.stok }}</td>
+            <td>Rp. {{ prod.price }}</td>
+            <td><button @click="addChart(index)">Add to chart</button></td>
+        </tr>
+    </table>
+
+</template>
+<script>
+
+export default({
+    emits:['emit-addChart'],
+    props:{
+        list:{
+            type:Array,
+            default:() => {
+                return []
+            }
+        }
+    },
+    data(){
+        return{
+            nom: null
+        }
+    },
+    methods:{
+        addChart(index){
+            this.$emit('emit-addChart', index)
+        }
+    }
+
+
+});
+
+</script>
