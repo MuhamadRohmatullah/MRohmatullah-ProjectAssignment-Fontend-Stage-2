5145c1c83cf08a4095d0c9ad43b70c8dc13b4902
MuhamadRohmatullah <m.rohmatullah10@gmail.com>
2024-02-06 18:57:40 +0700

    Membuat chart component dan menambahkan syntaks unutk menampilkan produk

---
+++ resources/js/components/ChartComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Quantity</th>
+                <th>Price</th>
+            </tr>
+        </thead> 
+        <tbody v-for="(flist, index) in foodList" :key="index">
+            <tr v-if="index == numIndex">
+                <td>{{ flist.name }}</td>
+                <td>{{ flist.stock }}</td>
+                <td>{{ flist.price }}</td>
+                <td><button>Delete</button></td>
+            </tr>
+        </tbody>
+        <tr>
+            <td>Total</td>
+            <td> </td>
+            <td>Rp.{{ }} </td>
+        </tr>
+        <tr>
+            <td><button>Checkout</button></td>
+        </tr>
+    </table>
+</template>
+<script>
+export default({
+    props:{
+        foodList:{
+            type:Array,
+            default:() => {
+                return []
+            }
+        },
+        numIndex:{
+            type:Number,
+            default: null
+        }
+    }
+})
+</script>
